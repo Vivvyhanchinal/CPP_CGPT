7aa352f8ad0efd72882df3a0b97d8dd8b335e2dc
Vivek Hanchinal <vivekhanchinal31@gmail.com>
2026-06-09 21:29:25 +0530

    Learned Variables and output in C++

---
+++ 01_Basics/variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+int main() {
+    int age = 20; 
+    float CGPA = 7.75; 
+    string name = "Vivek"; 
+    cout << name << endl; 
+    cout << age << endl ;
+    cout << CGPA << endl ; 
+    return 0;
+}
